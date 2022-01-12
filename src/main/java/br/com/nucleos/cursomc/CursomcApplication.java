9fac2f5d9618f3b57a32368649d28e1c884f6b02
José Carlos Rodrigues Lopes <jcarlosrl22@gmail.com>
2022-01-12 17:40:01 -0300

    Instanciação de entidades, criação de Produto e associação com categorias

--- src/main/java/br/com/nucleos/cursomc/CursomcApplication.java
+++ src/main/java/br/com/nucleos/cursomc/CursomcApplication.java
@@ -1,13 +1,49 @@
 package br.com.nucleos.cursomc;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.nucleos.cursomc.domain.Categoria;
+import br.com.nucleos.cursomc.domain.Produto;
+import br.com.nucleos.cursomc.repositories.CategoriaRepository;
+import br.com.nucleos.cursomc.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto prod1 = new Produto(null, "Computador", new BigDecimal("2000.00"));
+		Produto prod2 = new Produto(null, "Impressora", new BigDecimal("800.00"));
+		Produto prod3 = new Produto(null, "Mouse", new BigDecimal("80.00"));
+
+		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+
+		prod1.getCategorias().addAll(Arrays.asList(cat1));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		prod3.getCategorias().addAll(Arrays.asList(cat1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		this.produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
 	}
 
 }
--- src/main/java/br/com/nucleos/cursomc/domain/Produto.java
+++ src/main/java/br/com/nucleos/cursomc/domain/Produto.java
@@ -1,6 +1,7 @@
 package br.com.nucleos.cursomc.domain;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,27 +9,31 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
-
+public class Produto implements Serializable {
    private static final long serialVersionUID = 1L;
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
    private String nome;
+   private BigDecimal preco;
 
-   @ManyToMany(mappedBy = "categorias")
-   private List<Produto> produtos = new ArrayList<Produto>();
+   @ManyToMany
+   @JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+   private List<Categoria> categorias = new ArrayList<Categoria>();
 
-   public Categoria() {
+   public Produto() {
    }
 
-   public Categoria(Long id, String nome) {
+   public Produto(Long id, String nome, BigDecimal preco) {
       this.id = id;
       this.nome = nome;
+      this.preco = preco;
    }
 
    public Long getId() {
@@ -47,12 +52,20 @@ public class Categoria implements Serializable {
       this.nome = nome;
    }
 
-   public List<Produto> getProdutos() {
-      return produtos;
+   public BigDecimal getPreco() {
+      return preco;
    }
 
-   public void setProdutos(List<Produto> produtos) {
-      this.produtos = produtos;
+   public void setPreco(BigDecimal preco) {
+      this.preco = preco;
+   }
+
+   public List<Categoria> getCategorias() {
+      return categorias;
+   }
+
+   public void setCategorias(List<Categoria> categorias) {
+      this.categorias = categorias;
    }
 
    @Override
@@ -71,7 +84,7 @@ public class Categoria implements Serializable {
          return false;
       if (getClass() != obj.getClass())
          return false;
-      Categoria other = (Categoria) obj;
+      Produto other = (Produto) obj;
       if (id == null) {
          if (other.id != null)
             return false;
