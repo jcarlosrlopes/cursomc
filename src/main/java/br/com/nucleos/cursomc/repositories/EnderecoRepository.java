d14eeb8e2fc37d0fe550d8ba6e6b31f65d4f9876
José Carlos Rodrigues Lopes <jcarlosrl22@gmail.com>
2022-01-13 15:49:29 -0300

    Criação de clientes, tipo cliente, telefones e endereço

---
+++ src/main/java/br/com/nucleos/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package br.com.nucleos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.nucleos.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+
+}
