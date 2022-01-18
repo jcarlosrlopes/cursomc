3b1f5cb1e5a0bdf450b44d2e34448af116b243bc
José Carlos Rodrigues Lopes <jcarlosrl22@gmail.com>
2022-01-18 16:36:52 -0300

    Exclusão de categoria e tratamento de exceção para objetos associados

--- src/main/java/br/com/nucleos/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/nucleos/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,12 @@ public class CategoriaResource {
       URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(novaCategoria.getId())
             .toUri();
       return ResponseEntity.created(uri).build();
+   }
+
+   @DeleteMapping("{id}")
+   public ResponseEntity<Void> delete(@PathVariable Long id) {
+      this.service.deletar(id);
+      return ResponseEntity.noContent().build();
    }
 
 }
--- src/main/java/br/com/nucleos/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/nucleos/cursomc/services/CategoriaService.java
@@ -3,9 +3,11 @@ package br.com.nucleos.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.nucleos.cursomc.domain.Categoria;
+import br.com.nucleos.cursomc.exceptions.DataIntegrityException;
 import br.com.nucleos.cursomc.exceptions.ObjectNotFoundException;
 import br.com.nucleos.cursomc.repositories.CategoriaRepository;
 
@@ -24,6 +26,16 @@ public class CategoriaService {
    public Categoria criar(Categoria categoria) {
       categoria.setId(null);
       return this.repository.save(categoria);
+   }
+
+   public void deletar(Long id) {
+      buscar(id);
+
+      try {
+         repository.deleteById(id);
+      } catch (DataIntegrityViolationException e) {
+         throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+      }
    }
 
 }
