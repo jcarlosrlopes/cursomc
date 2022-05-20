7a8f666d667a4b86bd5577642052570307a85c21
José Carlos Rodrigues Lopes <jcarlosrl22@gmail.com>
2022-05-20 14:27:12 -0300

    Envio de email HTML

--- src/main/java/br/com/nucleos/cursomc/services/EmailService.java
+++ src/main/java/br/com/nucleos/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package br.com.nucleos.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.nucleos.cursomc.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
     void sendOrderConfirmationEmail(Pedido pedido);
 
     void sendEmail(SimpleMailMessage msg);
+
+    void sendOrderConfirmationHtmlEmail(Pedido pedido);
+
+    void sendHtmlEmail(MimeMessage msg);
 
 }
